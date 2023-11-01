a7e0391a254fb1abc0bce7943b86c8a0752f3255
seydaibro <seydaimam55@gmail.com>
2023-11-01 11:09:48 -0700

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <header>
+        TS Amazona
+      </header>
+      <main>
+      This is the main body
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+    
+  )
+}
+
+export default App
