5b1839927c1c1862603e8dfe4fe8098938887c5b
seydaibro <seydaimam55@gmail.com>
2023-11-01 12:17:28 -0700

    second commit

---
+++ client/src/models/Product.ts
@@ -0,0 +1,12 @@
+export default interface Product{
+    name: string
+    slug: string
+    image:string
+    category: string
+    brand: string
+    price: number
+    countInStock: number
+    decription: string
+    rating: number
+    nummReviews: number
+} 
